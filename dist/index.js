c0c0ee398ed6adfb5ff5b162f940fbcdae08bbb6
Alex Szabo <delanni.alex@gmail.com>
2024-07-09 15:34:31 +0000

    update dependencies

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.$if = void 0;
 var ifExpression = function (condition) {
     return makeIfStatement(condition);
 };
